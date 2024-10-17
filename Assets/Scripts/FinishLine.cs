f7df20f42a5dfd40c69f38f90584e75e9ebab04a
ThanhTungh <tungtottinh555@gmail.com>
2024-10-17 20:17:33 +0700

    Particale effect
    cach dung particle lam hieu ung

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float delayTime = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
+            finishEffect.Play();
             Invoke("ReloadScene", delayTime);
         }
         
