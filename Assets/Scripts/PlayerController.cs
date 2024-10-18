e07fa198c815d87baf6cf78dd8faa7ec32dcada3
ThanhTungh <tungtottinh555@gmail.com>
2024-10-18 15:06:57 +0700

    findobjectoftype
    dung de su dung cac component trong object khac tren cung 1 scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float speed = 5f;
+    [SerializeField] float rotateSpeed = 5f;
+    [SerializeField] float boost = 20f;
+    [SerializeField] float baseSpeed = 10f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)){
-            rb2d.AddTorque(speed);
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = boost;
         }
-        if (Input.GetKey(KeyCode.A)){
-            rb2d.AddTorque(-speed);
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(-rotateSpeed);
         }
     }
 }
